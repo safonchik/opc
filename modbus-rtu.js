91ff57e257385e30779ec69f662e0319f756e348
safonchik <a.s.safonchik@gmail.com>
2020-09-13 23:30:08 +0700

    first commit

--- modbus-rtu.js
+++ modbus-rtu.js
@@ -10,60 +10,32 @@ const defaultOptions = {
     stopbits: 1
 }
 
-class ModbusRTUClient {
+class ModbusClient {
     socket = null;
     client = null;
     // stream = new Readable({ read: () => true });
     dataStream = new EventEmitter();
     _opened = false;
-    constructor(port, id = 1, options = defaultOptions) {
+    constructor() {
+    }
+    connectTCP(port = 502, ip="127.0.0.1", id=1) {
+        this.socket = new net.Socket();
+        this.client = new Modbus.client.TCP(this.socket, id)
+        this.socket.connect({
+            'host': ip,
+            'port': port
+        });
+        this.socket.on("connect", () => {
+            this._opened = true;
+            console.log('TCP client opened');
+        })
+    }
+    connectRTU(port, id = 1, options = defaultOptions) {
         this.socket = new SerialPort(port, options)
         this.client = new Modbus.client.RTU(this.socket, id)
         this.socket.on("open", () => {
             this._opened = true;
             console.log('RTU client opened');
-        })
-    }
-
-    setListen(listen, interval = 500) {
-        setInterval(() => {
-            if (this._opened) {
-                listen.forEach((el) => {
-                    this.client[el.func](el.address, el.count).then((resp) => {
-                        const data = {
-                            id: el.id,
-                            timestamp: new Date(),
-                            func: el.func,
-                            address: el.address,
-                            count: el.count,
-                            value: resp.response.body.valuesAsArray
-                        }
-                        this.dataStream.emit(el.id, data)
-                    }).catch(() => {
-                        this.socket.close()
-                    })
-                });
-            }
-        }, interval)
-    }
-}
-
-class ModbusTCPClient {
-    socket = null;
-    client = null;
-    // stream = new Readable({ read: () => true });
-    dataStream = new EventEmitter();
-    _opened = false;
-    constructor(port, id = 1, options = defaultOptions) {
-        this.socket = new net.Socket();
-        this.client = new Modbus.client.TCP(this.socket, id)
-        this.socket.connect({
-            'host': '127.0.0.1',
-            'port': '502'
-        });
-        this.socket.on("connect", () => {
-            this._opened = true;
-            console.log('TCP client opened');
         })
     }
 
@@ -96,10 +68,10 @@ class ModbusTCPServer {
     server = null;
     _opened = false;
 
-    constructor() {
+    constructor(port = 502) {
         this.socket = net.Server();
         this.server = new Modbus.server.TCP(this.socket, { holding: Buffer.alloc(10000) });
-        this.socket.listen(502, '127.0.0.1');
+        this.socket.listen(port, '127.0.0.1');
         this.server.on("connection", () => {
             console.log('TCP server opened');
             this._opened = true;
@@ -113,9 +85,10 @@ class ModbusTCPServer {
 }
 
 
-const rtu = new ModbusRTUClient("COM5");
+const rtu = new ModbusClient();
 const tcp = new ModbusTCPServer();
-const client = new ModbusTCPClient();
+const client = new ModbusClient();
+rtu.connectRTU("COM5");
 rtu.setListen([
     { id: 'h0', func: "readHoldingRegisters", address: 0, count: 1 },
     // { id: 'i0', func: "readCoils", address: 3, count: 1 },
@@ -123,6 +96,7 @@ rtu.setListen([
 rtu.dataStream.subscribe("h0", data => {
     tcp.write(data.address, data.value)
 })
+client.connectTCP()
 client.setListen([
     { id: 'h0', func: "readHoldingRegisters", address: 0, count: 1 },
 ], 200)
